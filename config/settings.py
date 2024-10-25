97faf5deda50c85619afc165ff52277627dfe672
admin <codenamekirkpatrick@yandex.ru>
2024-10-25 17:37:56 +0300

    внесены настройки для работы с кэшем

--- config/settings.py
+++ config/settings.py
@@ -158,3 +158,12 @@ EMAIL_USE_SSL = True
 
 SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+CACHES_ENABLED = True
+if CACHES_ENABLED:
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
+            "LOCATION": "redis://127.0.0.1:6379",
+        }
+    }
